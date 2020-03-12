98289ffcdba72b6779a34c1f2b43b2726a869a09
winmo <win.moi@hotmail.com>
2020-03-12 12:10:03 +0100

    Version Linux et Windows

--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 from serial import *
 
 port_number: str
+type_programme: str
+type_OS: str
 
 
 def Fin():
@@ -13,8 +15,11 @@ def Fin():
 
 
 def Recoit(port: Serial):
-	ligne = port.read(128)
-	print(ligne)
+	while True:
+		ligne = None
+		ligne = port.read(128)
+		if ligne is not None:
+			print(ligne)
 
 
 def Envoie(port):
@@ -22,14 +27,23 @@ def Envoie(port):
 	port.write(nombre.encode('utf-8'))
 
 
+type_programme = input("Lecture (1) ou écriture (2) ?")
+type_OS = input("Linux (1) ou Windows (2) ?")
+
+
 port_number = input("N° port COM ? ")
-port_number = "COM" + port_number
+if type_OS == "1":
+	port_number = "/dev/tty" + port_number
+elif type_OS == "2":
+	port_number = "COM" + port_number
 
 with Serial(port=port_number, baudrate=9600, timeout=1, writeTimeout=1) as port_serie:
 	if port_serie.isOpen():
 		print("Port " + port_serie.port + " ouvert")
 
-		Envoie(port_serie)
-		Recoit(port_serie)
+		if type_programme == "1":
+			Envoie(port_serie)
+		elif type_programme == "2":
+			Recoit(port_serie)
 
 	Fin()
